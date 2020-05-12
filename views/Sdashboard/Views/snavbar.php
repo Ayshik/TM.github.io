ec9e2e4e481356dd5e5c9cac3323a8bfeb423cf0
MD WALI MOSNAD AYSHIK <61822224+Ayshik@users.noreply.github.com>
2020-05-12 22:12:32 +0600

    session distroyed

--- views/Sdashboard/Views/snavbar.php
+++ views/Sdashboard/Views/snavbar.php
@@ -126,7 +126,7 @@
        <div class="modal-body">Select "Logout" below if you are ready to Leave.</div>
        <div class="modal-footer">
          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-         <a class="btn btn-primary" href="../../Home.php">Logout</a>
+         <a class="btn btn-primary" href="logout.php">Logout</a>
        </div>
      </div>
    </div>
